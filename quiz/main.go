fc3b9a98c276ccf39d641137c93b00c2142263e0
Mitulsharma123 <sharmamitul72@gmail.com>
2023-10-17 19:17:26 +0530

    Logic added for csv file open,read

--- quiz/main.go
+++ quiz/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main(){
-	fmt.Println("Welcome to the quiz portal !!!")
+	csvfileName := flag.String("csv", "problems.csv", "a csv file in QnA format")
+	flag.Parse()
+
+
+	file, err := os.Open(*csvfileName)
+	if err != nil{
+		exit(fmt.Sprintf("failed to open the csv file %s\n", *csvfileName))
+	}
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err!=nil{
+		exit("Failed to parse the provided the csv file")
+	}
+	fmt.Println(lines)
+			
 }
+
+func exit(msg string){
+	fmt.Println(msg)
+	os.Exit(1)
+}
+
