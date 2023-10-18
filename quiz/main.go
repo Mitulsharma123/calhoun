a1eb9dca802aca92bd713dbdd887d6a4d06780f1
Mitulsharma123 <sharmamitul72@gmail.com>
2023-10-18 11:26:35 +0530

    add struct and parser function

--- quiz/main.go
+++ quiz/main.go
@@ -11,7 +11,6 @@ func main(){
 	csvfileName := flag.String("csv", "problems.csv", "a csv file in QnA format")
 	flag.Parse()
 
-
 	file, err := os.Open(*csvfileName)
 	if err != nil{
 		exit(fmt.Sprintf("failed to open the csv file %s\n", *csvfileName))
@@ -21,8 +20,26 @@ func main(){
 	if err!=nil{
 		exit("Failed to parse the provided the csv file")
 	}
-	fmt.Println(lines)
-			
+	fmt.Println(lines)			
+}
+	
+
+//parseLines will take input as 2D lines array and return slice of problems
+func parseLines(lines [][]string) []problem{
+	ret := make([]problem, len(lines))
+	for i, line := range lines{
+		ret[i] = problem{
+			q: line[0], //column 1
+			a: line[1], //column 2
+		}
+	}
+	return ret
+
+}
+
+type problem struct {
+	q string
+	a string
 }
 
 func exit(msg string){
