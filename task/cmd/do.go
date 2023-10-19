96d6fbd16609a35a895ba48035df11dc6e575695
Mitulsharma123 <sharmamitul72@gmail.com>
2023-10-19 22:40:41 +0530

    add logic for do list

--- task/cmd/do.go
+++ task/cmd/do.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"strconv"
 	"github.com/spf13/cobra"
 )
 
@@ -10,6 +11,16 @@ var doCmd = &cobra.Command{
 	Short: "Do a task added in your task list",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(" do called")
+		var ids []int
+		for _, arg := range args{
+			id, err := strconv.Atoi(arg)
+				if err!=nil{
+					fmt.Println("failed to parse args:", arg)
+				} else{
+					ids = append(ids, id)
+				}
+			}
+				fmt.Println(ids)
 	},
 }
 
