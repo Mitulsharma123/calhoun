54bc6dc22301a0fe7f77f54c0ba337cf1fb02a5a
Mitulsharma123 <sharmamitul72@gmail.com>
2023-10-20 14:56:58 +0530

    db connection

--- task/db/main.go
+++ task/db/main.go
@@ -1,20 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
 	"github.com/boltdb/bolt"
+	"time"
 )
 
 func main() {
-	// Open the my.db data file in your current directory.
-	// It will be created if it doesn't exist.
-	db, err := bolt.Open("my.db", 0600, nil)
-	fmt.Println("connection established....")
+	
+	db, err := bolt.Open("my.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
+	
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	defer db.Close()
-	fmt.Println("closing db connection....")
 }
