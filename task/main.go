e24dd83666168e5a65ee3f7fd0a5f50f461a3a5d
Mitulsharma123 <sharmamitul72@gmail.com>
2023-10-21 12:05:29 +0530

    adding all recent changes to homedir

--- task/main.go
+++ task/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "task/cmd"
+import (
+	"path/filepath"
+	"task/cmd"
+	"task/db"
+	"github.com/mitchellh/go-homedir"
+)
 
 func main(){
+	home, err := homedir.Dir()
+	if err!=nil{
+		panic(err)
+	} 
+	dbPath := filepath.Join(home, "tasks.db")
+	db.Init(dbPath)
+	if err!=nil{
+		panic(err)
+	}
 	cmd.RootCmd.Execute()
 }
+
+
