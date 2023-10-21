9cb43e507622f076b04c62079d371b1328d8e118
Mitulsharma123 <sharmamitul72@gmail.com>
2023-10-21 19:16:33 +0530

    replace byte.buffer with regex

--- normalizer/main.go
+++ normalizer/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
-	"bytes"
+	"regexp"
 )
 
 func normalize(phonenum string) string{
+	re := regexp.MustCompile("\\D")
+	return re.ReplaceAllString(phonenum, "")
+}
+
+
+/*func normalize(phonenum string) string{
 	var buf bytes.Buffer
 	for _, ch:= range phonenum{
 		if ch>='0' && ch <='9'{
@@ -12,4 +18,4 @@ func normalize(phonenum string) string{
 		}
 	}
 	return buf.String()
-}
+}*/
